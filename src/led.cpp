069b1f44e356cff6d2d0764220d4aebe9725f803
Thomaz Carleial <thomazcarleial@gmail.com>
2026-03-25 18:24:06 -0300

    arrumei o led.cpp e codei o locomotation.cpp

--- src/led.cpp
+++ src/led.cpp
@@ -10,17 +10,16 @@
 
 #include "led.hpp"
 
-Led::Led(GPIO_TypeDef* port, uint16_t pin, GPIO_PinState on_state) : port(port), pin(pin), on_state(on_state) {
+Led::Led(GPIO_TypeDef* port, uint16_t pin) {
     this->off();
 }
 
 void Led::on(void) {
-    HAL_GPIO_WritePin(this->port, this->pin, this->on_state);
+    HAL_GPIO_WritePin(this->port, this->pin);
 }
 
 void Led::off(void) {
-    GPIO_PinState off_state = ((this->on_state == GPIO_PIN_RESET) ? GPIO_PIN_SET : GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(this->port, this->pin, off_state);
+    HAL_GPIO_WritePin(this->port, this->pin);
 }
 
 void Led::toggle(void) {
--- src/locomotion.cpp
+++ src/locomotion.cpp
@@ -9,15 +9,18 @@
  */
 
 #include "locomotion.hpp"
+#include "motor.hpp"
 
 Locomotion::Locomotion(Motor& motor_left, Motor& motor_right) {
-    // Implemente aqui o criador da classe Locomotion.
+    this->motor_left = motor_left;
+    this->motor_right = motor_right;
 }
 
 void Locomotion::set_speed(int8_t left_speed, int8_t right_speed) {
-    // Implemente aqui a função para definir a velocidade da locomoção.
+    motor_left.set_speed(left_speed);
+    motor_right.set_speed(right_speed);
 }
 
 void Locomotion::stop() {
-    // Implemente aqui a função para parar a locomoção.
+motor_left.stop();
 }
