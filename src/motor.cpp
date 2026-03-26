4e2679ba5e58699285544c3e02736c66285274ca
Kawã Ricardo Bazante do Nascimento da Silva <kawabazante@gmail.com>
2026-03-26 10:49:17 -0300

    Refatora inicialização do construtor na classe Motor
    - Remove chamada redundante ao método tim_init() no construtor da classe Motor.
    - Mantém a lógica de inicialização do temporizador em uma única chamada, melhorando a clareza e a eficiência do código.
    
    Essa alteração foi realizada para evitar a execução desnecessária do método tim_init(), que já é chamado no corpo do construtor. A remoção da chamada duplicada contribui para uma melhor legibilidade e manutenção do código, além de prevenir possíveis efeitos colaterais indesejados que poderiam surgir de múltiplas inicializações.

--- src/motor.cpp
+++ src/motor.cpp
@@ -18,7 +18,6 @@ Motor::Motor(
     TIM_HandleTypeDef* backward_tim_handle, uint8_t backward_tim_ch, int8_t min_speed, int8_t max_speed
 ) : forward_tim_handle(forward_tim_handle), forward_tim_ch(forward_tim_ch), backward_tim_handle(backward_tim_handle), backward_tim_ch(backward_tim_ch), min_speed(min_speed), max_speed(max_speed)  {
 tim_init();
-tim_init();
 }
 
 void Motor::set_speed(int8_t speed) {
@@ -30,7 +29,6 @@ if(speed<0){
 else{
     __HAL_TIM_SET_COMPARE(this->forward_tim_handle,this->forward_tim_ch, map<uint32_t>(speed, this->min_speed, this->max_speed, 0,700));
     __HAL_TIM_SET_COMPARE(this->backward_tim_handle,this->backward_tim_ch, 0);
-
 }
 
 }
